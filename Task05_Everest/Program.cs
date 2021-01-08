041862287af4c85dd7d0f2eb6546bea704b2e635
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-08 19:49:30 +0200

    Add files via upload

---
+++ Task05_Everest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task05_Everest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int achievedHeight = 5364;
+            int day = 1;
+
+            while (achievedHeight < 8848)
+            {
+                string action = Console.ReadLine();
+                if (action == "END")
+                {
+                    break;
+                }
+                if (action == "Yes")
+                {
+                    day += 1;
+                }
+                
+                int nextHeight = int.Parse(Console.ReadLine());
+                achievedHeight += nextHeight;
+                if (day >= 5)
+                {
+                    break;
+                }
+            }
+            if (achievedHeight >= 8848)
+            {
+                Console.WriteLine($"Goal reached for {day} days!");
+            }
+            if (achievedHeight < 8848)
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(achievedHeight);
+            }
+        }
+    }
+}
