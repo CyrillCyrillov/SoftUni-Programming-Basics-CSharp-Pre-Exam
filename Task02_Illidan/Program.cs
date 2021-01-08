ad0a2cf912fdf38d379dac451db385b7958f28a6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-08 19:43:02 +0200

    Add files via upload

---
+++ Task02_Illidan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task02_Illidan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int oneManPower = int.Parse(Console.ReadLine());
+            int illidianPower = int.Parse(Console.ReadLine());
+
+            int peoplePower = people * oneManPower;
+
+            if (peoplePower >= illidianPower)
+            {
+                Console.WriteLine($"Illidan has been slain! You defeated him with {(peoplePower - illidianPower)} points.");
+            }
+            else
+            {
+                Console.WriteLine($"You are not prepared! You need {illidianPower - peoplePower} more points.");
+            }
+        }
+    }
+}
